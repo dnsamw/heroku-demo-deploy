3c4a5e121505258894e85c81f654d5ae7da6ebfa
Dileepa Samarawickrama <dnsam.mit@gmail.com>
2022-03-28 16:22:29 +0530

    set default script to webserver.py, add favicon

--- webserver.py
+++ webserver.py
@@ -1,19 +1,33 @@
-from flask import Flask
+from flask import Flask, send_from_directory
 from threading import Thread
+import os
 
-app = Flask('')
+
+app = Flask(__name__)
+
+
+@app.route('/favicon.ico')
+def favicon():
+    return send_from_directory(os.path.join(app.root_path, 'static'), 'favicon.ico', mimetype='image/favicon.png')
 
 
 @app.route('/')
+@app.route('/home')
 def home():
     return "I'm alive"
 
 
-def run():
-    #app.run(host='localhost', port=8080)
-    app.run(host='0.0.0.0', port=8080)
+# def run():
+#     #app.run(host='localhost', port=8080)
+#     app.run(host='0.0.0.0', port=8080)
 
 
-def keep_alive():
-    t = Thread(target=run)
-    t.start()
+# def keep_alive():
+#     t = Thread(target=run)
+#     t.start()
+
+
+if __name__ == '__main__':
+    app.secret_key = "ItIsASecret"
+    app.debug = True
+    app.run()
